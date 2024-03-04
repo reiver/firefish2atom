07c6c458719210c6c1ca2b457b4065738f793639
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-04 12:51:19 -0800

    errors

---
+++ firefisharchive/errors.go
@@ -0,0 +1,10 @@
+package firefisharchive
+
+import (
+	"sourcecode.social/reiver/go-erorr"
+)
+
+const (
+	errNilReceiver = erorr.Error("firefish2html: nil receiver")
+	errNilWriter   = erorr.Error("firefish2html: nil writer")
+)
